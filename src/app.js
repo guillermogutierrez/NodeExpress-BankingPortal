5ebb707ef79f4cc3f84259c5dfb86061bcc8855e
guillermo <guillermo.gutierrez-tamargo@vwfs.co.uk>
2019-10-29 22:58:11 +0000

    Block 4 Completed

--- src/app.js
+++ src/app.js
@@ -2,17 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+const data = require('./data.js');
+const users = data.users
+const accounts = data.accounts
+const writeJSON = data.writeJSON
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
-
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
@@ -23,16 +20,14 @@ app.get('/transfer', (req, res) => res.render('transfer', { user: users[0] }));
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    var accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', { 'message': 'Transfer Completed' });
 });
 app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10);
-    var accountsJSON = JSON.stringify(accounts);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('payment', { 'message': 'Payment Successful', 'account': accounts.credit });
 });
 app.use(express.urlencoded({ extended: true }));
